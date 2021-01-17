d250fb041d64e8e2ed07d28913ada85841d1b4bd
Moripho <72516446+Moripho@users.noreply.github.com>
2021-01-17 19:02:42 +0100

    Überarbeitung der Kapitelaabgabe 3

--- AbgabeKapitel3/serverscript.js
+++ AbgabeKapitel3/serverscript.js
@@ -1,30 +1,30 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.P_3_1Server = void 0;
-const Http = require("http");                                               // importieren des HTTP-Moduls, was für den Serverbau gebraucht wird
+const Http = require("http"); // importieren des HTTP-Moduls, was für den Serverbau gebraucht wird
 var P_3_1Server;
 (function (P_3_1Server) {
-    console.log("Starting server");                                         //Konsolenausgabe "Starting Server" um Startpunk des Servers in der Konsolenausgabe nachvollziehen zu können
-    let port = Number(process.env.PORT);                                    // Erstellen der Port-Adresse
-    if (!port)                                                              // falls kein Port existiert, wird dem Port der Wert 8100 (localhost) zugewiesen
+    console.log("Starting server"); //Konsolenausgabe "Starting Server" um Startpunk des Servers in der Konsolenausgabe nachvollziehen zu können
+    let port = Number(process.env.PORT); // Erstellen der Port-Adresse
+    if (!port) // falls port keinen Wert hat, wird der Port 8100 (localhost) zugewiesen
         port = 8100;
-    let server = Http.createServer();                                       // createServer() erstellt einen Server und speicher Ergebnis in der Variablen "server"
-    server.addListener("request", handleRequest);                           // Um Anfragen von Nutzern auf einem Server verarbeiten zu können wird dieser Eventlistener verwendet. Der Listener ruft für jede eingehende Nutzeranfrage die handleRequest-Funktion auf.
-    server.addListener("listening", handleListen);                          // Eventhandler für das "Zuhören" des Servers. Hört der Server zu und es erfolgt in dem Momement keine Anfrage durch den Nutzer, so wird die Funktion handleListen ausgeführt 
-    server.listen(port);                                                    // Server soll auf Portnummer hören
-    function handleListen() {                                               // Funktion handleListen
-        console.log("Listening");                                           // Konsolenausgabe "Listening", die dem Nutzer veranschaulicht, dass der Server gerade "zuhört"
+    let server = Http.createServer(); // createServer() erstellt einen Server und speichert dessen Wert in der Variablen server vom Typ HTML.server
+    server.addListener("request", handleRequest); // Um Anfragen von Nutzern auf einem Server verarbeiten zu können, wird dieser Eventlistener verwendet. Der Listener ruft für jede eingehende Nutzeranfrage bzw. request die handleRequest-Funktion auf  
+    server.addListener("listening", handleListen); // Eventhandler für das Zuhören des Servers. Hört Server zu und befindet sich im status "listen" und es erfolgt keine Anfrage durch den Nutzer, so wird die Funktion handleListen() aufgerufen
+    server.listen(port); // Server soll auf vorher definierte Portnummer hören
+    function handleListen() {
+        console.log("Listening"); // Konsolenausgabe, die obiges wiederspiegeln soll
     }
-    function handleRequest(_request, _response) {                           // Funktion, die die Serveranfragen durch Nutzer verarbeitet.
-                                                                            // LAUT VORLESUNGSMATERIALIEN:
-                                                                            // Die Funktion erwartet normalerweise zwei Parameter: den ersten vom Typ IncomingMessage, den zweiten vom Typ ServerResponse (beide aus dem http-Modul).
-                                                                            //IncomingMessage liefert Infos zum eingegangenen Request-Objekt (z. B. URL als String).
-                                                                            //ServerResponse ist ein Objekt, welches Infos für die Antwort sammelt. Die Info wird in zwei Kategorien aufgeteilt: Header (Infos zur eigentlichen Nachricht) + Body (die Nachricht selbst)
-        console.log("I hear voices!");                                      // Konsolenausgabe "I hear voices", wenn Server eine Nutzeranfrage verarbeitet
-        _response.setHeader("content-type", "text/html; charset=utf-8");    // über setHeader()-Funktion wird Header-Information integriert. Header gibt an, dass die Serverantwort ein mit utf-8 kodierter Text ist       
-        _response.setHeader("Access-Control-Allow-Origin", "*");            // jeder darf Nachricht öffnen, Asterisk "*" = alles
-        _response.write(_request.url);                                      // Funktion, die dem Nachrichten-Body die URL der Serverrequest anfügt
-        _response.end();
+    function handleRequest(_request, _response) {
+        // LAUT VORLESUNGSMATERIALIEN:
+        // handleRequest erwartet normalerweise zwei Parameter, ersteres vom Typ IncomingMessage, letzteres vom Typ ServerResponse (beide aus http-Modul)
+        // IncomingMessage liefert Infos zum eingegangenen Request-Objekt (z. B. URL als String)
+        // ServerResponse ist ein Objekt, welches Infos für die Serverantowrt sammelt. Die Info wird unterteilt in Header (Infos zur eigentlichen Nachricht) und Body (die Nachricht selbst)                                                                                                  
+        console.log("I hear voices!"); // Konsolenausgabe "I hear voices", wenn Server eine Nutzeranfrage verarbeitet
+        _response.setHeader("content-type", "text/html; charset=utf-8"); // über setHeader-Funktion wird Header-Information integriert. Header gibt an, dass die Serverantwort ein mit utf-8 kodierter Text ist.
+        _response.setHeader("Access-Control-Allow-Origin", "*"); // jeder darf Nachricht öffnen, Asterisk "*" = alles
+        _response.write(_request.url); // Funktion, die dem Nachrichten-Body die URL der Serverrequest anfügt
+        _response.end(); // markiert Ende der Serverantwort
     }
 })(P_3_1Server = exports.P_3_1Server || (exports.P_3_1Server = {}));
 //# sourceMappingURL=serverscript.js.map
--- AbgabeKapitel3/serverscript.ts
+++ AbgabeKapitel3/serverscript.ts
@@ -1,26 +1,30 @@
-import * as Http from "http"; // importieren des HTTP-Moduls, was für den Serverbau gebraucht wird
+import * as Http from "http";                                                                       // importieren des HTTP-Moduls, was für den Serverbau gebraucht wird
 
-export namespace P_3_1Server { // Namespacing für den Server der Kapitelaufgabe 3.1
-    console.log("Starting server"); //Konsolenausgabe "Starting Server" um Startpunk des Servers in der Konsolenausgabe nachvollziehen zu können
-    let port: number = Number(process.env.PORT); // Erstellen der Port-Adresse
-    if (!port) // falls kein Port existiert, wird der Port 8100 (localhost) erstellt
+export namespace P_3_1Server {                                                                      // Namespacing für den Server der Kapitelaufgabe 3.1
+    console.log("Starting server");                                                                 //Konsolenausgabe "Starting Server" um Startpunk des Servers in der Konsolenausgabe nachvollziehen zu können
+    let port: number = Number(process.env.PORT);                                                    // Erstellen der Port-Adresse
+    if (!port)                                                                                      // falls port keinen Wert hat, wird der Port 8100 (localhost) zugewiesen
         port = 8100;
 
-    let server: Http.Server = Http.createServer(); // Erstellen des Http-Servers
-    server.addListener("request", handleRequest); // Eventhandler für Serverrequest 
-    server.addListener("listening", handleListen);
-    server.listen(port);                            // Server 
+    let server: Http.Server = Http.createServer();                                                  // createServer() erstellt einen Server und speichert dessen Wert in der Variablen server vom Typ HTML.server
+    server.addListener("request", handleRequest);                                                   // Um Anfragen von Nutzern auf einem Server verarbeiten zu können, wird dieser Eventlistener verwendet. Der Listener ruft für jede eingehende Nutzeranfrage bzw. request die handleRequest-Funktion auf  
+    server.addListener("listening", handleListen);                                                  // Eventhandler für das Zuhören des Servers. Hört Server zu und befindet sich im status "listen" und es erfolgt keine Anfrage durch den Nutzer, so wird die Funktion handleListen() aufgerufen
+    server.listen(port);                                                                            // Server soll auf vorher definierte Portnummer hören
 
-    function handleListen(): void { // 
-        console.log("Listening");
+    function handleListen(): void { //                                                              // Funktion die angibt, dass Server gerade zuhört
+        console.log("Listening");                                                                   // Konsolenausgabe, die obiges wiederspiegeln soll
     }
 
 
-    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void { // Funktion, für die Serverrequest
-        console.log("I hear voices!");
-        _response.setHeader("content-type", "text/html; charset=utf-8");
-        _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.write(_request.url);
-        _response.end();
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {  // Funktion, die die Serveranfragen durch Nutzer verarbeitet
+                                                                                                    // LAUT VORLESUNGSMATERIALIEN:
+                                                                                                    // handleRequest erwartet normalerweise zwei Parameter, ersteres vom Typ IncomingMessage, letzteres vom Typ ServerResponse (beide aus http-Modul)
+                                                                                                    // IncomingMessage liefert Infos zum eingegangenen Request-Objekt (z. B. URL als String)
+                                                                                                    // ServerResponse ist ein Objekt, welches Infos für die Serverantowrt sammelt. Die Info wird unterteilt in Header (Infos zur eigentlichen Nachricht) und Body (die Nachricht selbst)                                                                                                  
+        console.log("I hear voices!");                                                              // Konsolenausgabe "I hear voices", wenn Server eine Nutzeranfrage verarbeitet
+        _response.setHeader("content-type", "text/html; charset=utf-8");                            // über setHeader-Funktion wird Header-Information integriert. Header gibt an, dass die Serverantwort ein mit utf-8 kodierter Text ist.
+        _response.setHeader("Access-Control-Allow-Origin", "*");                                    // jeder darf Nachricht öffnen, Asterisk "*" = alles
+        _response.write(_request.url);                                                              // Funktion, die dem Nachrichten-Body die URL der Serverrequest anfügt
+        _response.end();                                                                            // markiert Ende der Serverantwort
     }
 }
