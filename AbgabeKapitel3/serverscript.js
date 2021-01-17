51be2181dfc8e79b2f4f999b2cb5fb5fae635f9e
Moripho <72516446+Moripho@users.noreply.github.com>
2021-01-17 17:53:05 +0100

    Überarbeitung Aufgabe 3.1

--- AbgabeKapitel3/serverscript.js
+++ AbgabeKapitel3/serverscript.js
@@ -6,14 +6,14 @@ var P_3_1Server;
 (function (P_3_1Server) {
     console.log("Starting server"); //Konsolenausgabe "Starting Server" um Startpunk des Servers in der Konsolenausgabe nachvollziehen zu können
     let port = Number(process.env.PORT); // Erstellen der Port-Adresse
-    if (!port) // falls kein Port existiert, wird der Port 8100 (localhost) erstellt
+    if (!port) // falls kein Port existiert, wird dem Port der Wert 8100 (localhost) zugewiesen
         port = 8100;
-    let server = Http.createServer(); // erstellt einen Server
+    let server = Http.createServer(); // createServer() erstellt einen Server und speicher Ergebnis in der Variablen "server"
     server.addListener("request", handleRequest); // Um Anfragen von Nutzern auf einem Server verarbeiten zu können wird dieser Eventlistener verwendet. Der Listener ruft für jede eingehende Nutzeranfrage die handleRequest-Funktion auf.
-    server.addListener("listening", handleListen); // Eventhandler für das "Zuhören" des Servers, bei einer Serverrequest wird die Funktion handleRequest ausgeführt 
-    server.listen(port); // Server 
-    function handleListen() {
-        console.log("Listening");
+    server.addListener("listening", handleListen); // Eventhandler für das "Zuhören" des Servers. Hört der Server zu und es erfolgt in dem Momement keine Anfrage durch den Nutzer, so wird die Funktion handleListen ausgeführt 
+    server.listen(port); // Server soll auf Portnummer hören
+    function handleListen() { // Funktion handleListen
+        console.log("Listening");   // Konsolenausgabe "Listening", die dem Nutzer veranschaulicht, dass der Server gerade "zuhört"
     }
     function handleRequest(_request, _response) { // Funktion, die die Serveranfragen durch Nutzer verarbeitet
         console.log("I hear voices!");              // Konsolenausgabe "I hear voices"
