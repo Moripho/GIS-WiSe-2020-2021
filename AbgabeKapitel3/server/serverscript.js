f266a2132686626f7fde301f1b06f9b5f74b8d0b
Moripho <72516446+Moripho@users.noreply.github.com>
2021-01-24 20:19:51 +0100

    Kapitelabgabe 3

--- AbgabeKapitel3/server/serverscript.js
+++ AbgabeKapitel3/server/serverscript.js
@@ -87,6 +87,7 @@ var KapitelabgabeDreiServer;
     }
     async function getUsers() {
         const users = await userData.find({}, { projection: { _id: 0, fname: 1, lname: 1, postalCode: 0, city: 0, email: 0, password: 0 } }).toArray();
+        console.log(users);
         return JSON.stringify({
             error: false,
             message: JSON.stringify(users)
--- AbgabeKapitel3/server/serverscript.ts
+++ AbgabeKapitel3/server/serverscript.ts
@@ -116,6 +116,7 @@ export namespace KapitelabgabeDreiServer {                                      
     async function getUsers(): Promise<string> {
         const users: User[] = await userData.find({}, { projection: { _id: 0, fname: 1, lname: 1, postalCode: 0, city: 0, email: 0, password: 0 } }).toArray();
 
+        console.log(users);
         return JSON.stringify({
             error: false,
             message: JSON.stringify(users)
